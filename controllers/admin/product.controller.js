dffe17fe8eecb7c60d808ea81d47f45163a41f33
Trần Tuấn Anh <91252961+khaihuen@users.noreply.github.com>
2026-02-13 22:22:03 +0700

    Tối ưu tính năng part 2

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const sortHelper = require("../../helpers/sort");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -19,24 +20,9 @@ module.exports.index = async (req, res) => {
     }
 
     // Sort
-    const sort = {};
-    if (req.query.sortKey && req.query.sortValue) {
-        sort[req.query.sortKey] = req.query.sortValue === "desc" ? -1 : 1;
-    } else {
-        sort.position = -1;
-    }
+    const objectSort = sortHelper(req.query);
 
-    // Sort Options
-    const sortOptions = [
-        { value: "position-desc", label: "Vị trí giảm dần" },
-        { value: "position-asc", label: "Vị trí tăng dần" },
-        { value: "price-desc", label: "Giá giảm dần" },
-        { value: "price-asc", label: "Giá tăng dần" },
-        { value: "title-asc", label: "Tiêu đề A - Z" },
-        { value: "title-desc", label: "Tiêu đề Z - A" }
-    ];
-
-    const products = await Product.find(find).sort(sort);
+    const products = await Product.find(find).sort(objectSort.sortObject);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
@@ -44,6 +30,6 @@ module.exports.index = async (req, res) => {
         products: products,
         filterStatus: filterStatus,
         keyword: objectSearch.keyword,
-        sortOptions: sortOptions
+        sortOptions: objectSort.sortOptions
     });
 }
--- helpers/sort.js
+++ helpers/sort.js
@@ -1,14 +1,23 @@
 module.exports = (query) => {
-    let sort = {
-        position: "desc",
-        key: "position",
-        value: "desc"
-    };
+    const sortOptions = [
+        { value: "position-desc", label: "Vị trí giảm dần" },
+        { value: "position-asc", label: "Vị trí tăng dần" },
+        { value: "price-desc", label: "Giá giảm dần" },
+        { value: "price-asc", label: "Giá tăng dần" },
+        { value: "title-asc", label: "Tiêu đề A - Z" },
+        { value: "title-desc", label: "Tiêu đề Z - A" }
+    ];
+
+    const sortObject = {};
 
     if(query.sortKey && query.sortValue) {
-        sort.key = query.sortKey;
-        sort.value = query.sortValue;
+        sortObject[query.sortKey] = query.sortValue === "desc" ? -1 : 1;
+    } else {
+        sortObject.position = -1;
     }
 
-    return sort;
+    return {
+        sortObject: sortObject,
+        sortOptions: sortOptions
+    };
 }
